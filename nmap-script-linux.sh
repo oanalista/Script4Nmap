6524fe3c08d27fd9e35596db149172163ed5f519
O Analista <falecom@oanalista.com.br>
2016-07-26 16:20:52 -0300

    Update nmap-script-linux.sh

--- nmap-script-linux.sh
+++ nmap-script-linux.sh
@@ -5,7 +5,7 @@ tempo=$(date +"%d_%m_%Y-%H:%M:%S")
 if [ -z $1 ]  || [ -z $2 ]
 then
    echo "Favor informar o numero da porta e a lista de IPs!"
-   echo "Use ./nmap-script-linux.sh <PORTA> <ARQUIVO COM OS IPs>"
+   echo "Use ./portscan.sh <PORTA> <ARQUIVO COM OS IPs>"
 else
 
 clear
@@ -23,41 +23,36 @@ echo ""
 Inicio=$(date +%s.%N);
 
 # Lendo cada endereco IP existente no arquivo
-for i in $( cat /opt/scans/$2 ); do
+cat $2 | while read host; do
 
-# Roda o Nmap em cada IP encontrado no arquivo e redireciona o resultado para um arquivo de log
-     nmap -P0 -p $1 $i >> /opt/scans/scanreport-$tempo.log
+# Roda o Nmap em cada IP encontrado no arquivo
 
+     STATUS=$(nmap -P0 -p $1 $host | grep tcp | awk '{print $2}')
 
-# Procurando por determinada string dentro do arquivo de log
-     STATUS1=$(egrep -i 'open' /opt/scans/scanreport-$tempo.log | wc -l)	
-     STATUS2=$(egrep -i 'closed' /opt/scans/scanreport-$tempo.log | wc -l)
-     STATUS3=$(egrep -i 'filtered' /opt/scans/scanreport-$tempo.log | wc -l)
-
-     if [ $STATUS1 = 1 ]
-     then	
-		echo "Servidor: $i | Porta: $1 | Status: ABERTA"
-		echo "Data e hora da varredura: $tempo" >> /opt/scans/status-UP-$tempo.log
-		echo "Servidor: $i | Porta: $1 | Status: ABERTA" >> /opt/scans/status-UP-$tempo.log
-		echo "**************************************************" >> /opt/scans/status-UP-$tempo.log
-     fi
-	
-     if [ $STATUS2 = 1 ]
+     if [ $STATUS == open ]
      then
-		echo "Servidor: $i | Porta: $1 | Status: FECHADA"
-		echo "Data e hora da varredura: $tempo" >> /opt/scans/status-DOWN-$tempo.log
-		echo "Servidor: $i | Porta: $1 | Status: FECHADA" >> /opt/scans/status-DOWN-$tempo.log
-		echo "**************************************************" >> /opt/scans/status-DOWN-$tempo.log
+                echo "Servidor: $host | Porta: $1 | Status: ABERTA"
+                echo "Data e hora da varredura: $tempo" >> /opt/scans/status-UP-$tempo.log
+                echo "Servidor: $host | Porta: $1 | Status: ABERTA" >> /opt/scans/status-UP-$tempo.log
+                echo "**************************************************" >> /opt/scans/status-UP-$tempo.log
      fi
-   
-     if [ $STATUS3 = 1 ]
+
+     if [ $STATUS = closed ]
      then
-                echo "Servidor: $i | Porta: $1 | Status: FILTRADA"
+                echo "Servidor: $host | Porta: $1 | Status: FECHADA"
+                echo "Data e hora da varredura: $tempo" >> /opt/scans/status-DOWN-$tempo.log
+                echo "Servidor: $host | Porta: $1 | Status: FECHADA" >> /opt/scans/status-DOWN-$tempo.log
+                echo "**************************************************" >> /opt/scans/status-DOWN-$tempo.log
+     fi
+
+     if [ $STATUS = filtered ]
+     then
+                echo "Servidor: $host | Porta: $1 | Status: FILTRADA"
                 echo "Data e hora da varredura: $tempo" >> /opt/scans/status-FILTRADA-$tempo.log
-                echo "Servidor: $i | Porta: $1 | Status: FECHADA" >> /opt/scans/status-FILTRADA-$tempo.log
+                echo "Servidor: $host | Porta: $1 | Status: FECHADA" >> /opt/scans/status-FILTRADA-$tempo.log
                 echo "**************************************************" >> /opt/scans/status-FILTRADA-$tempo.log
      fi
-       
+
 done
 dur=$(echo "$(date +%s.%N) - $Inicio" | bc);
 echo ""
